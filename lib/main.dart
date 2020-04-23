427400f0e0aa15f241c94d3aa0f5d8d49937e589
Justin Eltoft <Justin.Eltoft@idealindustries.com>
2020-04-23 09:08:20 -0500

    cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -132,7 +132,8 @@ class _MyHomePageState extends State<MyHomePage> {
                 exit(0);
               }
             },
-            itemBuilder: (BuildContext context) => <PopupMenuEntry<OptionChoice>>[
+            itemBuilder: (BuildContext context) =>
+                <PopupMenuEntry<OptionChoice>>[
               const PopupMenuItem<OptionChoice>(
                 value: OptionChoice.reset,
                 //child: Icon(Icons.restore),
@@ -152,176 +153,176 @@ class _MyHomePageState extends State<MyHomePage> {
         ],
       ),
       body: WillPopScope(
-    onWillPop: _stopAll,  // prevent the back button for now, must kill app
-    child:Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Column(
-                  children: <Widget>[
-                    SizedBox(
-                      width: 160,
-                      height: 160,
+        onWillPop: _stopAll, // prevent the back button for now, must kill app
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      SizedBox(
+                        width: 160,
+                        height: 160,
+                        child: RaisedButton(
+                          textColor: Colors.white,
+                          color: _awayTimeByPeriod[_currentPeriod].isRunning
+                              ? Colors.green[300]
+                              : Colors.red[200],
+                          child: Text(
+                            "THEM",
+                            style: TextStyle(fontSize: 30),
+                          ),
+                          elevation: 10,
+                          onPressed: () => _toggleAwayTimer(_currentPeriod),
+                          shape: new RoundedRectangleBorder(
+                            borderRadius: new BorderRadius.circular(10.0),
+                          ),
+                        ),
+                      ),
+                      Container(height: 50),
+                      TimerText(
+                        sw: _awayTimeByPeriod[0],
+                        period: 1,
+                      ),
+                      Container(height: 20),
+                      TimerText(
+                        sw: _awayTimeByPeriod[1],
+                        period: 2,
+                      ),
+                      Container(height: 20),
+                      TimerText(
+                        sw: _awayTimeByPeriod[2],
+                        period: 3,
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      SizedBox(
+                        width: 160,
+                        height: 160,
+                        child: RaisedButton(
+                          textColor: Colors.white,
+                          color: _homeTimeByPeriod[_currentPeriod].isRunning
+                              ? Colors.green[300]
+                              : Colors.red[200],
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Image.asset('assets/viperhead.png'),
+                              //Text( "Vipers", style: TextStyle(fontSize: 30), ),
+                            ],
+                          ),
+                          elevation: 10,
+                          onPressed: () => _toggleHomeTimer(_currentPeriod),
+                          shape: new RoundedRectangleBorder(
+                            borderRadius: new BorderRadius.circular(10.0),
+                          ),
+                        ),
+                      ),
+                      Container(height: 50),
+                      TimerText(
+                        sw: _homeTimeByPeriod[0],
+                        period: 1,
+                      ),
+                      Container(height: 20),
+                      TimerText(
+                        sw: _homeTimeByPeriod[1],
+                        period: 2,
+                      ),
+                      Container(height: 20),
+                      TimerText(
+                        sw: _homeTimeByPeriod[2],
+                        period: 3,
+                      ),
+                    ],
+                  )
+                ],
+              ),
+              Container(
+                height: 30,
+              ),
+              Text("Choose Period(long press)"),
+              Container(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  SizedBox(
+                    width: 70,
+                    height: 70,
+                    child: GestureDetector(
+                      onLongPress: () => _updatePeriod(0),
                       child: RaisedButton(
+                        onPressed: () => print("press 1"),
                         textColor: Colors.white,
-                        color: _awayTimeByPeriod[_currentPeriod].isRunning
-                            ? Colors.green[300]
-                            : Colors.red[200],
+                        color: _currentPeriod == 0
+                            ? Colors.blue[300]
+                            : Colors.grey[300],
                         child: Text(
-                          "THEM",
+                          "1",
                           style: TextStyle(fontSize: 30),
                         ),
-                        elevation: 10,
-                        onPressed: () => _toggleAwayTimer(_currentPeriod),
-                        shape: new RoundedRectangleBorder(
-                          borderRadius: new BorderRadius.circular(10.0),
+                        elevation: 8,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0),
                         ),
                       ),
                     ),
-                    Container(height: 50),
-                    TimerText(
-                      sw: _awayTimeByPeriod[0],
-                      period: 1,
-                    ),
-                    Container(height: 20),
-                    TimerText(
-                      sw: _awayTimeByPeriod[1],
-                      period: 2,
-                    ),
-                    Container(height: 20),
-                    TimerText(
-                      sw: _awayTimeByPeriod[2],
-                      period: 3,
-                    ),
-                  ],
-                ),
-                Column(
-                  children: <Widget>[
-                    SizedBox(
-                      width: 160,
-                      height: 160,
+                  ),
+                  SizedBox(
+                    width: 70,
+                    height: 70,
+                    child: GestureDetector(
+                      onLongPress: () => _updatePeriod(1),
                       child: RaisedButton(
+                        onPressed: () => print("press 2"),
                         textColor: Colors.white,
-                        color: _homeTimeByPeriod[_currentPeriod].isRunning
-                            ? Colors.green[300]
-                            : Colors.red[200],
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            Image.asset('assets/viperhead.png'),
-                            //Text( "Vipers", style: TextStyle(fontSize: 30), ),
-                          ],
+                        color: _currentPeriod == 1
+                            ? Colors.blue[300]
+                            : Colors.grey[300],
+                        child: Text(
+                          "2",
+                          style: TextStyle(fontSize: 30),
                         ),
-                        elevation: 10,
-                        onPressed: () => _toggleHomeTimer(_currentPeriod),
-                        shape: new RoundedRectangleBorder(
-                          borderRadius: new BorderRadius.circular(10.0),
+                        elevation: 8,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0),
                         ),
                       ),
                     ),
-                    Container(height: 50),
-                    TimerText(
-                      sw: _homeTimeByPeriod[0],
-                      period: 1,
-                    ),
-                    Container(height: 20),
-                    TimerText(
-                      sw: _homeTimeByPeriod[1],
-                      period: 2,
-                    ),
-                    Container(height: 20),
-                    TimerText(
-                      sw: _homeTimeByPeriod[2],
-                      period: 3,
-                    ),
-                  ],
-                )
-              ],
-            ),
-            Container(
-              height: 30,
-            ),
-            Text("Choose Period(long press)"),
-            Container(
-              height: 10,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                SizedBox(
-                  width: 70,
-                  height: 70,
-                  child: GestureDetector(
-                    onLongPress: () => _updatePeriod(0),
-                    child: RaisedButton(
-                      onPressed: () => print("press 1"),
-                      textColor: Colors.white,
-                      color: _currentPeriod == 0
-                          ? Colors.blue[300]
-                          : Colors.grey[300],
-                      child: Text(
-                        "1",
-                        style: TextStyle(fontSize: 30),
-                      ),
-                      elevation: 8,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  SizedBox(
+                    width: 70,
+                    height: 70,
+                    child: GestureDetector(
+                      onLongPress: () => _updatePeriod(2),
+                      child: RaisedButton(
+                        onPressed: () => print("press 3"),
+                        textColor: Colors.white,
+                        color: _currentPeriod == 2
+                            ? Colors.blue[300]
+                            : Colors.grey[300],
+                        child: Text(
+                          "3",
+                          style: TextStyle(fontSize: 30),
+                        ),
+                        elevation: 8,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
                       ),
                     ),
                   ),
-                ),
-                SizedBox(
-                  width: 70,
-                  height: 70,
-                  child: GestureDetector(
-                    onLongPress: () => _updatePeriod(1),
-                    child: RaisedButton(
-                      onPressed: () => print("press 2"),
-                      textColor: Colors.white,
-                      color: _currentPeriod == 1
-                          ? Colors.blue[300]
-                          : Colors.grey[300],
-                      child: Text(
-                        "2",
-                        style: TextStyle(fontSize: 30),
-                      ),
-                      elevation: 8,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  width: 70,
-                  height: 70,
-                  child: GestureDetector(
-                    onLongPress: () => _updatePeriod(2),
-                    child: RaisedButton(
-                      onPressed: () => print("press 3"),
-                      textColor: Colors.white,
-                      color: _currentPeriod == 2
-                          ? Colors.blue[300]
-                          : Colors.grey[300],
-                      child: Text(
-                        "3",
-                        style: TextStyle(fontSize: 30),
-                      ),
-                      elevation: 8,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            )
-          ],
+                ],
+              )
+            ],
+          ),
         ),
-      ),
       ),
       /*
       floatingActionButton: FloatingActionButton(
@@ -413,8 +414,8 @@ class TimerTextState extends State<TimerText> {
   @override
   Widget build(BuildContext context) {
     //String theTime = sprintf("P%d%s%02d:%02d", [widget.period, "\u2192", minutes, seconds]);
-    String theTime =
-        sprintf("%02d:%02d.%01d", [minutes, seconds % 60, ((hundreds%100)~/10)]);
+    String theTime = sprintf(
+        "%02d:%02d.%01d", [minutes, seconds % 60, ((hundreds % 100) ~/ 10)]);
     return new Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
